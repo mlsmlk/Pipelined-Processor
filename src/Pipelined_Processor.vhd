69c5dc352855b59b2c92daf3a96d99ee5ba58186
Noah-C-Z <nczwack@gmail.com>
2021-04-20 22:52:08 -0400

    Add execute stage to processor

--- src/Pipelined_Processor.vhd
+++ src/Pipelined_Processor.vhd
@@ -92,7 +92,45 @@ architecture arch of pipelined_processor is
     end component;
 
     --- EXECUTE ---
-    -- ***TODO***
+    component execute is
+        port(
+            --- INPUTS ---
+            -- From the Decode state
+            e_insttype : in std_logic_vector(1 downto 0);
+            e_readdata1 : in std_logic_vector(31 downto 0);
+            e_readdata2 : in std_logic_vector(31 downto 0);
+            e_imm : in std_logic_vector(31 downto 0);
+            -- funct if R; opcode for I and J
+            e_opcode : in std_logic_vector(5 downto 0);
+            -- Clock
+            clock : in std_logic;
+            -- From the Fetch stage
+            f_reset : in std_logic;
+            f_nextPC : in std_logic_vector(31 downto 0); -- PC+4
+            -- Forwarding
+            -- Signal to Execute to use the forwarded value from Execute
+            e_forward_ex : in std_logic;
+            -- Indicate which operand the forwarded value from Execute maps to
+            -- "10" = readdata1 || "01" = readdata2 || "11" = both
+            e_forwardop_ex : in std_logic_vector(1 downto 0);
+            -- Signal to Execute to use the forwarded value from Memory
+            e_forward_mem : in std_logic;
+            -- Indicate which operand the forwarded value from Memory maps to
+            -- "10" = readdata1 || "01" = readdata2 || "11" = both
+            e_forwardop_mem : in std_logic_vector(1 downto 0);
+            -- Forwarded data from memory
+            m_forward_data : in std_logic_vector(31 downto 0);
+            
+            --- OUTPUTS ---
+            -- To the Memory stage
+            alu_result : out std_logic_vector(31 downto 0);
+            writedata : out std_logic_vector(31 downto 0);
+            readwrite_flag : out std_logic_vector(1 downto 0);
+            -- Branch
+            branch_taken : out STD_LOGIC;
+            branch_target_addr : out STD_LOGIC_VECTOR (31 downto 0)
+        );
+    end component;
 
     --- MEMORY ---
     component data_memory is
@@ -140,8 +178,8 @@ begin
         -- Inputs
         clock,
         reset,
-        jump_address => ???,
-        jump_flag => ???,
+        jump_address => branch_target_addr,
+        jump_flag => branch_taken,
         stall_pipeline => f_stall,
         -- Outputs
         instruction => f_instruction,
@@ -154,36 +192,58 @@ begin
     port map(
         -- Inputs
         clock,
-        write_reg_file => ???,
+        write_reg_file => write_to_file,
         f_instruction => instruction,
         f_reset => reset_out,
         f_pcplus4 => program_counter_out,
         w_regdata => write_data,
         -- Outputs
         f_stall => stall_pipeline,
-        e_insttype => ???,
-        e_opcode => ???,
-        e_readdata1 => ???,
-        e_readdata2 => ???,
-        e_imm => ???,
-        e_forward_ex => ???,
-        e_forwardop_ex => ???,
-        e_forward_mem => ???,
-        e_forwardop_mem => ???
+        e_insttype,
+        e_opcode,
+        e_readdata1,
+        e_readdata2,
+        e_imm,
+        e_forward_ex,
+        e_forwardop_ex,
+        e_forward_mem,
+        e_forwardop_mem
     );
 
     --- EXECUTE ---
-    -- ***TODO***
+    ex : decode
+    port map(
+        -- Inputs
+        clock,
+        e_insttype,
+        e_readdata1,
+        e_readdata2,
+        e_imm,
+        e_opcode,
+        f_reset => reset_out,
+        f_nextPC => program_counter_out,
+        e_forward_ex,
+        e_forwardop_ex,
+        e_forward_mem,
+        e_forwardop_mem,
+        m_forward_data => mem_res,
+        -- Outputs
+        alu_result => alu_in,
+        writedata => mem_in,
+        readwrite_flag,
+        branch_taken => jump_flag,
+        branch_target_addr => jump_address
+    );
 
     --- MEMORY ---
     mem : data_memory
     port map(
         -- Inputs
         clock,
-        alu_in => ???,
-        mem_in => ???,
-        readwrite_flag => ???,
-        write_file_flag => ???,
+        alu_in => alu_result,
+        mem_in => writedata,
+        readwrite_flag,
+        write_file_flag => write_to_file,
         -- Outputs
         mem_res,
         mem_flag,
